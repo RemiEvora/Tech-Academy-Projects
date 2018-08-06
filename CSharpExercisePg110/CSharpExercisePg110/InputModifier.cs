751775836aa0918f6331af0fe4c66b1fd5d2291c
RemiEvora <39279111+RemiEvora@users.noreply.github.com>
2018-08-06 14:14:10 -0500

    C# & .NET Framework Course Exercise Pg 110
    Created a simple calculator that requests two numeric inputs from the user, then conducts addition, multiplication and division operations on the input and prints the resulting output.

---
+++ CSharpExercisePg110/CSharpExercisePg110/InputModifier.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercisePg110
+{
+    public class InputModifier
+    {
+       
+        public InputModifier()
+        {
+
+        }
+
+        public double Addition(double numInput1, double numInput2)
+
+        { 
+            return numInput1 + numInput2;
+        }
+
+        public double Multiplication(double numInput1, double numInput2)
+
+        {
+            return numInput1 * numInput2;
+        }
+
+        public double Division(double numInput1, double numInput2)
+
+        {
+            return numInput1 / numInput2;
+        }
+
+    }
+}
---
+++ CSharpExercisePg110/CSharpExercisePg110/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercisePg110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputModifier Calculator = new InputModifier();
+            double result1;
+            double result2;
+            double result3;
+            
+
+            Console.WriteLine("Give me two numbers of your choice, and I'll run some calculations on them.");
+            
+            //Request user for first number 
+            Console.WriteLine("What do you want to use for your first number?"); 
+            double numInput1 = Convert.ToDouble(Console.ReadLine());
+
+            //Request user for second number 
+            Console.WriteLine("Ok. What about your second number?");
+            double numInput2 = Convert.ToDouble(Console.ReadLine());
+
+            //First Input Modification 
+            result1 = Calculator.Addition(numInput1, numInput2);
+            Console.WriteLine("Ok. So, adding " + numInput1 + " and " + numInput2 + " equals " + result1 + ".");
+            Console.ReadLine();
+
+            //Second Input Modification
+            result2 = Calculator.Multiplication(numInput1, numInput2);
+            Console.WriteLine("Then, multiplying " + numInput1 + " and " + numInput2 + " will give you " + result2 + ".");
+            Console.ReadLine();
+
+            //Third Input Modificication 
+            result3 = Calculator.Division(numInput1, numInput2);
+            Console.WriteLine("Finally, if you divide " + numInput1 + " by " + numInput2 + " you get " + result3 + ".");
+            Console.ReadLine(); 
+            
+        }
+    }
+}
