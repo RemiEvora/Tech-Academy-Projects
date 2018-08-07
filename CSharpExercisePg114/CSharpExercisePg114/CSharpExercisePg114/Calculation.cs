b91290cfad02a8ba60e84a2d63b8121a2fcf3186
RemiEvora <39279111+RemiEvora@users.noreply.github.com>
2018-08-07 14:42:26 -0500

    C# & .NET Framework Exercise Pg 114
    Created a code that takes two integers as parameters, where the second int parameter is optional for entry. 
    
    Currently functional with exceptions -  trying to figure out how to make it work if no user input is given for the 2nd parameter request is given, attempting several different methods to no avail at the moment.

---
+++ CSharpExercisePg114/CSharpExercisePg114/CSharpExercisePg114/Calculation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercisePg114
+{
+    public class Calculation
+    {
+        public Calculation()
+        {
+
+        }
+
+        public int TwoIntAddition(int num01, int optionalNum02 = 10)
+        {
+            
+            return num01 + optionalNum02; 
+        }
+    }
+}
---
+++ CSharpExercisePg114/CSharpExercisePg114/CSharpExercisePg114/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercisePg114
+{
+    class Program
+    {
+        static void Main()
+        {
+            Calculation Calculator = new Calculation();
+            
+
+            Console.WriteLine("Give me a number: ");
+            int num01 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Give me one more number. If you don't feel like it, just press the '0'/zero key.");
+            int optionalNum02 = Convert.ToInt32(Console.ReadLine());
+            
+            if (!int.TryParse(optionalNum02, out int defaultNum02))
+                defaultNum02 = 10; 
+            
+            Console.WriteLine(Calculator.TwoIntAddition(num01));
+            Console.ReadLine(); 
+
+
+
+        }
+    }
+}
+
+
+//IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+//1. Create a class. In that class, create a method that takes two integers as parameters.Make one of them optional.Have the method do a math operation and return a integer result.
+
+//2. In the Main() method of the console app, instantiate the class.
+
+//3. Ask the user to input two numbers, one at a time.Let them know they need not enter anything for the second number.
+
+//4. Call the method in the class, passing in the one or two numbers entered.
+
+//5. Try various combinations of numbers on the code, including having no second number.
