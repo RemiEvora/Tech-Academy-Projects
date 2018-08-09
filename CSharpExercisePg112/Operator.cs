ec0048f80896cc79170c0b5e8fb7a45ee762ba1e
RemiEvora <39279111+RemiEvora@users.noreply.github.com>
2018-08-09 10:48:31 -0500

    C# & .NET Framework Course Exercise 112
    (rework) Created a console app that took two Int parameters, ran them through a void method that conducts a mathematical operation on them, but only displays the 2nd int parameter, regardless of the result of the math operation. 
    
    Reworked the code to function by commenting-out the "void" and adding a datatype for the method, as well as adding a return for correct output.

---
+++ CSharpExercisePg112/Operator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercisePg112
+{
+
+
+
+    public class Operator
+    {
+      public Operator()
+        {
+            
+        }
+
+        //Create a class. In that class, create a void method that takes two integers as parameters. 
+        //Have the method do a math operation on the first integer and display the second integer to the screen.
+        public /*void*/ int Subtraction(int num01, int num02)
+        {
+            
+            int result = num01 - 5;
+            //return (num02); -> Testing method for functionality with public int subtraction + return command. 
+            return num02;
+        }
+    }
+}
---
+++ CSharpExercisePg112/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercisePg112
+{
+    class Program
+    {
+        
+        static void Main()
+        {
+            //Instantiate class. 
+            Operator Calculator = new Operator();
+
+
+            //Call the method in the class, passing in two numbers.
+            int answer = Calculator.Subtraction(5, 2);  
+
+
+            //Call the method in the class, specifying the parameters by name. 
+            int answer2 = Calculator.Subtraction(num01: 20, num02: 15);
+
+            Console.WriteLine(answer);
+            Console.WriteLine(answer2);
+            Console.ReadLine(); 
+        }
+
+    }
+}
+
+
+//There are couple things wrong with this drill:
+//1) The reason that you are getting this error is that a void method is not returning a value. That is what the 'void' classification means when creating this method.
+//2) Your method has no datatype defined and is not returning a value to be assigned to the int variables in your Program.cs. 
+
+//Try reworking your code and resubmitting based on the notes given above. If you need to discuss further, please let me know. I'd be more than happy to help.  - Cole
