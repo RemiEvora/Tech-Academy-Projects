aef9de7bd2182599910c44e84efa2b2ac20382ad
RemiEvora <39279111+RemiEvora@users.noreply.github.com>
2018-08-13 14:54:36 -0500

    C# & .NET Framework Course Exercise Pg 128
    Using the previous console app from Pg 119, 123 and 125, added overloaded ==/!= operators to check if Employee IDs matched or not  per exercise requirements.

---
+++ CSharpExercisePg128/CSharpExercisePg128/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercisePg128
+{
+    class Employee : Person, IQuittable
+    {
+        public int EmployeeID { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Employee Name: ");
+            base.SayName();
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Warning or guidance message here.");
+            Console.ReadLine();
+        }
+
+        //=====Overload the '==' operator so it checks if two Employee objects are equal by comparing their ID property=====//
+        public static bool operator ==(Employee NewEmp1, Employee NewEmp2)
+        {
+            
+            {
+                return NewEmp1.EmployeeID == NewEmp2.EmployeeID;
+            }
+             
+        }
+
+        public static bool operator !=(Employee NewEmp1, Employee NewEmp2)
+        {
+           
+            
+            {
+                return NewEmp1.EmployeeID != NewEmp2.EmployeeID;
+            }
+            
+        }
+
+        public override int GetHashCode()
+        {
+            return EmployeeID.GetHashCode();
+        }
+    }
+}
---
+++ CSharpExercisePg128/CSharpExercisePg128/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercisePg128
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee Example = new Employee
+            //{
+            //    FirstName = "Sample",
+            //    LastName = "Student"
+            //};
+
+            //Example.SayName();
+
+             
+            //IQuittable QuittingPerson = new Employee();
+            //QuittingPerson.Quit();
+
+            //=====Overload the '==' operator so it checks if two Employee objects are equal by comparing their ID property=====//
+            Employee NewEmp1 = new Employee();
+            NewEmp1.EmployeeID = 000000;
+
+            Employee NewEmp2 = new Employee();
+            NewEmp2.EmployeeID = 000001;
+
+            Console.WriteLine(NewEmp1 == NewEmp2);
+            Console.ReadLine(); 
+
+
+
+        }
+    }
+}
