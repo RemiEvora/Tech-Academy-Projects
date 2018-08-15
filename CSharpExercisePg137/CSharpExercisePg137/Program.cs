42130a58b594a5c686bfe8e2da21b06a8b070a41
RemiEvora <39279111+RemiEvora@users.noreply.github.com>
2018-08-15 11:57:09 -0500

    C# & .NET Framework Exercise Pg 137
    Created a console app to practice structs.

---
+++ CSharpExercisePg137/CSharpExercisePg137/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercisePg137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.In the Main() method, create an object of data type Number and assign an amount to it.
+            Number Num1 = new Number(); 
+            Num1.Amount = 3500.50M;
+
+
+            //3.Print this amount to the console.
+            Console.WriteLine(Num1.Amount);
+            Console.ReadLine(); 
+        }
+    }
+}
