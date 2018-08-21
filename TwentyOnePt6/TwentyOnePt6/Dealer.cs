345b0a745c0416f5d4a0f525c64390952c1af1f0
RemiEvora <39279111+RemiEvora@users.noreply.github.com>
2018-08-21 10:31:21 -0500

    Casino Twenty One/Blackjack Tutorial Game
    Console App Tutorial designed to mimic a casino blackjack game (incomplete)

---
+++ TwentyOnePt6/TwentyOnePt6/Dealer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; 
+
+namespace TwentyOnePt6
+{
+    public class Dealer
+    {
+        public string Name { get; set; }
+        public Deck Deck { get; set; }
+        public int Balance { get; set; }
+
+
+        public void Deal(List<Card> Hand)
+        {
+            Hand.Add(Deck.Cards.First());
+            //========APPEND TEXT========// 
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+            Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Rembrandt.Evora\Desktop\2018 Tech Academy Bootcamp\C# & .NET Framework Course Documents\logtest.txt", true))
+            {
+                //StreamWriter takes path of log, and "true" argument is so that it 
+                //it appends, if "false", creates new file
+                file.WriteLine(card); //To write 'string card' line 21, to the file. 
+            } //Once program hits this bracket, it clears the memory via the 'using' statement, rather than accumulate in memory storage
+            Deck.Cards.RemoveAt(0);
+
+
+        }
+    }
+}
---
+++ TwentyOnePt6/TwentyOnePt6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TwentyOnePt6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //=======READ IN A FILE TEXT=======//
+            //string text = File.ReadAllText(@"C:\Users\Rembrandt.Evora\Desktop\2018 Tech Academy Bootcamp\C# & .NET Framework Course Documents\logtest.txt"); //Reading back previously created file. 
+
+
+            //=======WRITE IN A FILE TEXT=======//
+            //string text = "Here is some text.";
+            //File.WriteAllText(@"C:\Users\Rembrandt.Evora\Desktop\2018 Tech Academy Bootcamp\C# & .NET Framework Course Documents\logtest.txt", text); //If 'logtest.txt' does not exist, it will create it or overwrite a current version of it. 
+            //If you do not have the '@' symbol, the address will require double \\ symbols, -> C:\\Users\\ etc. 
+
+
+            Console.WriteLine("Welcome to the Grand Hotel & Casino. Let's start by telling me your name.");
+            string playerName = Console.ReadLine();
+
+            Console.WriteLine("And how much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.IsActivelyPlaying = true; 
+                while (player.IsActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing."); 
+
+            }
+            Console.WriteLine("Feel free to look around the casino. Bye for now.");
+            Console.Read(); 
+        }
+    }
+}
